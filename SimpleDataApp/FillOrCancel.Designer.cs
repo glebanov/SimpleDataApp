51bcfe1aa1b519614d4e9b5802a4426a639c9990
Nikolai Glebanov <n.glebanov@gmail.com>
2022-08-31 16:19:52 +0300

    Добавьте файлы проекта.

---
+++ SimpleDataApp/FillOrCancel.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace SimpleDataApp
+{
+    partial class FillOrCancel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOrderID = new System.Windows.Forms.TextBox();
+            this.btnFindByOrderID = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpFillDate = new System.Windows.Forms.DateTimePicker();
+            this.dgvCustomerOrders = new System.Windows.Forms.DataGridView();
+            this.btnCancelOrder = new System.Windows.Forms.Button();
+            this.btnFillOrder = new System.Windows.Forms.Button();
+            this.btnFinishUpdates = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(16, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Идентификатор заказа:";
+            // 
+            // txtOrderID
+            // 
+            this.txtOrderID.Location = new System.Drawing.Point(228, 29);
+            this.txtOrderID.Name = "txtOrderID";
+            this.txtOrderID.Size = new System.Drawing.Size(131, 22);
+            this.txtOrderID.TabIndex = 1;
+            // 
+            // btnFindByOrderID
+            // 
+            this.btnFindByOrderID.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnFindByOrderID.Location = new System.Drawing.Point(422, 22);
+            this.btnFindByOrderID.Name = "btnFindByOrderID";
+            this.btnFindByOrderID.Size = new System.Drawing.Size(148, 33);
+            this.btnFindByOrderID.TabIndex = 2;
+            this.btnFindByOrderID.Text = "Найти заказ";
+            this.btnFindByOrderID.UseVisualStyleBackColor = true;
+            this.btnFindByOrderID.Click += new System.EventHandler(this.btnFindByOrderID_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(16, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(389, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "При заполнении заказа укажите дату заполнения ";
+            // 
+            // dtpFillDate
+            // 
+            this.dtpFillDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFillDate.Location = new System.Drawing.Point(422, 79);
+            this.dtpFillDate.Name = "dtpFillDate";
+            this.dtpFillDate.Size = new System.Drawing.Size(148, 22);
+            this.dtpFillDate.TabIndex = 4;
+            // 
+            // dgvCustomerOrders
+            // 
+            this.dgvCustomerOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomerOrders.Location = new System.Drawing.Point(112, 206);
+            this.dgvCustomerOrders.Name = "dgvCustomerOrders";
+            this.dgvCustomerOrders.ReadOnly = true;
+            this.dgvCustomerOrders.RowHeadersVisible = false;
+            this.dgvCustomerOrders.Size = new System.Drawing.Size(602, 150);
+            this.dgvCustomerOrders.TabIndex = 5;
+            // 
+            // btnCancelOrder
+            // 
+            this.btnCancelOrder.BackColor = System.Drawing.Color.Red;
+            this.btnCancelOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCancelOrder.ForeColor = System.Drawing.Color.Snow;
+            this.btnCancelOrder.Location = new System.Drawing.Point(112, 375);
+            this.btnCancelOrder.Name = "btnCancelOrder";
+            this.btnCancelOrder.Size = new System.Drawing.Size(129, 36);
+            this.btnCancelOrder.TabIndex = 6;
+            this.btnCancelOrder.Text = "Отменить заказ";
+            this.btnCancelOrder.UseVisualStyleBackColor = false;
+            this.btnCancelOrder.Click += new System.EventHandler(this.btnCancelOrder_Click);
+            // 
+            // btnFillOrder
+            // 
+            this.btnFillOrder.BackColor = System.Drawing.Color.Olive;
+            this.btnFillOrder.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFillOrder.ForeColor = System.Drawing.Color.Snow;
+            this.btnFillOrder.Location = new System.Drawing.Point(340, 375);
+            this.btnFillOrder.Name = "btnFillOrder";
+            this.btnFillOrder.Size = new System.Drawing.Size(124, 36);
+            this.btnFillOrder.TabIndex = 7;
+            this.btnFillOrder.Text = "Заполнить заказ";
+            this.btnFillOrder.UseVisualStyleBackColor = false;
+            this.btnFillOrder.Click += new System.EventHandler(this.btnFillOrder_Click);
+            // 
+            // btnFinishUpdates
+            // 
+            this.btnFinishUpdates.BackColor = System.Drawing.Color.Blue;
+            this.btnFinishUpdates.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFinishUpdates.ForeColor = System.Drawing.Color.Snow;
+            this.btnFinishUpdates.Location = new System.Drawing.Point(562, 375);
+            this.btnFinishUpdates.Name = "btnFinishUpdates";
+            this.btnFinishUpdates.Size = new System.Drawing.Size(144, 34);
+            this.btnFinishUpdates.TabIndex = 8;
+            this.btnFinishUpdates.Text = "Готово";
+            this.btnFinishUpdates.UseVisualStyleBackColor = false;
+            this.btnFinishUpdates.Click += new System.EventHandler(this.btnFinishUpdates_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtOrderID);
+            this.groupBox1.Controls.Add(this.btnFindByOrderID);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.dtpFillDate);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(112, 32);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(602, 150);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Поиск заказа";
+            // 
+            // FillOrCancel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnFinishUpdates);
+            this.Controls.Add(this.btnFillOrder);
+            this.Controls.Add(this.btnCancelOrder);
+            this.Controls.Add(this.dgvCustomerOrders);
+            this.Name = "FillOrCancel";
+            this.Text = "Заполнение или отмена заказов";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOrderID;
+        private System.Windows.Forms.Button btnFindByOrderID;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpFillDate;
+        private System.Windows.Forms.DataGridView dgvCustomerOrders;
+        private System.Windows.Forms.Button btnCancelOrder;
+        private System.Windows.Forms.Button btnFillOrder;
+        private System.Windows.Forms.Button btnFinishUpdates;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
---
+++ SimpleDataApp/Navigation.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace SimpleDataApp
+{
+    partial class Navigation
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnGoToAdd = new System.Windows.Forms.Button();
+            this.btnGoToFillOrCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(298, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(209, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберите действие";
+            // 
+            // btnGoToAdd
+            // 
+            this.btnGoToAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGoToAdd.Location = new System.Drawing.Point(62, 144);
+            this.btnGoToAdd.Name = "btnGoToAdd";
+            this.btnGoToAdd.Size = new System.Drawing.Size(270, 70);
+            this.btnGoToAdd.TabIndex = 1;
+            this.btnGoToAdd.Text = "Добавить учетную запись";
+            this.btnGoToAdd.UseVisualStyleBackColor = true;
+            this.btnGoToAdd.Click += new System.EventHandler(this.btnGoToAdd_Click);
+            // 
+            // btnGoToFillOrCancel
+            // 
+            this.btnGoToFillOrCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnGoToFillOrCancel.Location = new System.Drawing.Point(477, 144);
+            this.btnGoToFillOrCancel.Name = "btnGoToFillOrCancel";
+            this.btnGoToFillOrCancel.Size = new System.Drawing.Size(270, 70);
+            this.btnGoToFillOrCancel.TabIndex = 2;
+            this.btnGoToFillOrCancel.Text = "Заполнить или отменить заказ";
+            this.btnGoToFillOrCancel.UseVisualStyleBackColor = true;
+            this.btnGoToFillOrCancel.Click += new System.EventHandler(this.btnGoToFillOrCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(335, 271);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(141, 47);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Выйти";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Navigation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnGoToFillOrCancel);
+            this.Controls.Add(this.btnGoToAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Navigation";
+            this.Text = "Приветствие";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGoToAdd;
+        private System.Windows.Forms.Button btnGoToFillOrCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
